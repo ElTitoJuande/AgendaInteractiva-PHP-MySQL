11a2e38666a39c67161bd8cb6e82be8d3bf84301
unknown <juandepm00@gmai.com>
2025-02-12 09:40:19 +0100

    Actualizando base de datos

--- SQL/agenda_final.sql
+++ SQL/agenda_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-02-2025 a las 13:01:21
+-- Tiempo de generación: 12-02-2025 a las 09:35:18
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -40,21 +40,32 @@ CREATE TABLE `amigos` (
 --
 
 INSERT INTO `amigos` (`id`, `nombre`, `apellidos`, `fecha_nac`, `usuario`) VALUES
-(1, 'bbb', 'bbb', '2012-12-12', 1),
-(2, 'Antonio', 'Roldan', '1999-01-14', 4),
-(3, 'Senior', 'Saenz', '2002-11-16', 1),
-(4, 'Pacho', 'Rodriguez', '2003-01-10', 4),
-(5, 'Ana', 'Maite', '2004-12-21', 4),
-(6, 'Alejandro', 'Aguayo', '2002-10-14', 6),
-(7, 'Fernando', 'Benitez', '2001-03-20', 4),
-(8, 'Aaaaaaaaaa', 'asdfasdf', '2025-02-03', 4),
-(9, 'Angel', 'Arevalo', '2011-11-11', 3),
-(10, 'Angel', 'ElOreon', '2011-11-11', 4),
-(15, 'Alexiillo', 'tutupa', '2011-11-11', 1),
-(16, 'asdfasdfasdf', 'fdasfada', '2012-12-12', 3),
-(17, 'gggggggggggg', 'gg', '2012-12-12', 3),
-(18, 'Papote', 'Gaspar', '1939-09-10', 3),
-(19, 'Papote', 'Gaspar', '1939-09-10', 4);
+(32, 'Pablo', 'Duran', '2012-12-12', 1),
+(33, 'Antonio', 'Roldan', '1999-01-14', 4),
+(34, 'Senior', 'Saenz', '2002-11-16', 1),
+(35, 'Pacho', 'Rodriguez', '2003-01-10', 4),
+(36, 'Ana', 'Maite', '2004-12-21', 4),
+(37, 'Maria', 'Gonzalez', '2001-05-15', 1),
+(38, 'Carlos', 'Jimenez', '1998-08-23', 1),
+(39, 'Laura', 'Martinez', '2000-03-30', 4),
+(40, 'Diego', 'Sanchez', '2002-07-19', 4),
+(41, 'Carmen', 'Lopez', '1999-04-25', 4),
+(42, 'Manuel', 'Garcia', '2001-09-14', 3),
+(43, 'Isabel', 'Fernandez', '2000-06-18', 3),
+(44, 'Jorge', 'Torres', '2002-03-22', 3),
+(45, 'Lucia', 'Morales', '1998-11-30', 5),
+(46, 'Roberto', 'Herrera', '2003-08-05', 5),
+(47, 'Elena', 'Castro', '2001-12-11', 1),
+(48, 'Miguel', 'Ruiz', '1999-07-28', 1),
+(49, 'Sofia', 'Ortiz', '2002-05-15', 3),
+(50, 'Daniel', 'Flores', '2000-02-19', 3),
+(51, 'Paula', 'Vega', '2003-10-07', 4),
+(52, 'Fernando', 'Reyes', '1998-01-16', 4),
+(53, 'Andrea', 'Medina', '2001-06-29', 5),
+(54, 'Javier', 'Silva', '2002-04-12', 5),
+(55, 'Natalia', 'Romero', '2000-09-23', 1),
+(56, 'Alejandro', 'Vargas', '1999-12-08', 3),
+(57, 'asd', 'asd', '2025-02-28', 1);
 
 -- --------------------------------------------------------
 
@@ -81,12 +92,25 @@ INSERT INTO `juegos` (`id`, `titulo`, `plataforma`, `lanzamiento`, `img`, `usuar
 (3, 'EAFC25', 'PlayStation 4', '2024', 'eafc25.jpg', 4),
 (4, 'The Legend of Zelda: Tears of the Kingdom', 'Nintendo Switch', '2023', 'zelda.jpg', 4),
 (5, 'God of War', 'PlayStation 4', '2018', 'godOfWar.jpg', 4),
-(6, 'Red Dead Redemption 2', 'PlayStation 4', '2018', 'rdr2.jpg', 4),
-(12, 'asdf', 'pc', '2025', 'AnotaciÃ³n 2025-02-10 111601.png', 4),
-(13, 'asdf', 'pc', '2024', 'AnotaciÃ³n 2025-02-10 111601.png', 4),
-(14, 'asdf', 'pc', '2000', 'AnotaciÃ³n 2025-02-10 111601.png', 4),
-(15, 'fdas', 'PLayStation4', '2001', 'logo1.png', 4),
-(16, 'asdfaasdf', 'pc', '2011', 'logo1.png', 4);
+(6, 'Red Dead Redemption 2', 'PlayStation 4', '2018', 'RedDeadRedemptionII.jpg', 4),
+(17, 'Fortnite', 'PC', '2017', 'fortnite.jpg', 1),
+(18, 'Minecraft', 'PC', '2009', 'minecraft.jpg', 1),
+(19, 'EAFC25', 'PlayStation 4', '2024', 'eafc25.jpg', 1),
+(20, 'The Legend of Zelda: Tears of the Kingdom', 'Nintendo Switch', '2023', 'zelda.jpg', 1),
+(21, 'God of War', 'PlayStation 4', '2018', 'godOfWar.jpg', 1),
+(22, 'Red Dead Redemption 2', 'PlayStation 4', '2018', 'RedDeadRedemptionII.jpg', 1),
+(23, 'Fortnite', 'PC', '2017', 'fortnite.jpg', 3),
+(24, 'Minecraft', 'PC', '2009', 'minecraft.jpg', 3),
+(25, 'EAFC25', 'PlayStation 4', '2024', 'eafc25.jpg', 3),
+(26, 'The Legend of Zelda: Tears of the Kingdom', 'Nintendo Switch', '2023', 'zelda.jpg', 3),
+(27, 'God of War', 'PlayStation 4', '2018', 'godOfWar.jpg', 3),
+(28, 'Red Dead Redemption 2', 'PlayStation 4', '2018', 'RedDeadRedemptionII.jpg', 3),
+(29, 'Fortnite', 'PC', '2017', 'fortnite.jpg', 5),
+(30, 'Minecraft', 'PC', '2009', 'minecraft.jpg', 5),
+(31, 'EAFC25', 'PlayStation 4', '2024', 'eafc25.jpg', 5),
+(32, 'The Legend of Zelda: Tears of the Kingdom', 'Nintendo Switch', '2023', 'zelda.jpg', 5),
+(33, 'God of War', 'PlayStation 4', '2018', 'godOfWar.jpg', 5),
+(34, 'Red Dead Redemption 2', 'PlayStation 4', '2018', 'RedDeadRedemptionII.jpg', 5);
 
 -- --------------------------------------------------------
 
@@ -108,9 +132,14 @@ CREATE TABLE `prestamos` (
 --
 
 INSERT INTO `prestamos` (`id`, `usuario`, `amigo`, `juego`, `fecha_prestamo`, `devuelto`) VALUES
-(1, 1, 1, 1, '2025-01-02', 0),
-(2, 1, 3, 3, '2025-01-15', 1),
-(3, 4, 4, 2, '2001-11-11', 1);
+(13, 1, 37, 4, '2025-01-20', 0),
+(14, 1, 38, 5, '2025-02-01', 1),
+(15, 3, 43, 2, '2025-01-10', 0),
+(16, 3, 44, 6, '2025-01-25', 1),
+(17, 4, 33, 3, '2025-01-15', 1),
+(18, 4, 36, 1, '2025-02-05', 1),
+(19, 5, 45, 4, '2025-01-30', 0),
+(20, 5, 46, 5, '2025-02-10', 1);
 
 -- --------------------------------------------------------
 
@@ -132,10 +161,9 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `nombre`, `contrasena`, `tipo`) VALUES
 (1, 'Erica', '123456', 'usuario'),
 (2, 'Juande', '123456', 'admin'),
-(3, 'Redouan', '123465', 'usuario'),
+(3, 'Redouan', '123456', 'usuario'),
 (4, 'Fran', '123456', 'usuario'),
-(5, 'alexillo', 'tutu', 'usuario'),
-(6, 'Papote Gaspote', '123456', '');
+(5, 'alexillo', '123456', 'usuario');
 
 --
 -- Índices para tablas volcadas
@@ -182,19 +210,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `amigos`
 --
 ALTER TABLE `amigos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT de la tabla `juegos`
 --
 ALTER TABLE `juegos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
